fe933a107831919a45b239abbdf30886962fb152
Mike Hockerman <mike@hockerman.com>
2024-12-02 11:53:50 -0500

    day01: read input from fs

--- day01/day01_test.go
+++ day01/day01_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestDay01(t *testing.T) {
-	output, err := Day01()
+	output, err := Day01("./input")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,6 +18,13 @@ func TestDay01(t *testing.T) {
 	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0)
 	fmt.Fprintf(tw, termcolor.LightGreen("Day 01\tPart 1\tTotal Distance\t%v")+"\n", output.TotalDistance)
 	fmt.Fprintf(tw, termcolor.LightGreen("Day 01\tPart 2\tSimilarity Score\t%v")+"\n", output.SimilarityScore)
-	fmt.Fprintf(tw, termcolor.LightGreen("--\t--\tDuration\t%v")+"\n", output.Duration)
+	fmt.Fprintf(tw, termcolor.LightGreen("--\t--\tDuration\t%vμs")+"\n", output.Duration.Microseconds())
 	tw.Flush()
+
+	if output.TotalDistance != 2113135 {
+		t.Fatalf("invalid total distance (%v) for provided input, expected %v", output.TotalDistance, 2113135)
+	}
+	if output.SimilarityScore != 19097157 {
+		t.Fatalf("invalid similarity score (%v) for provided input, expected %v", output.SimilarityScore, 19097157)
+	}
 }

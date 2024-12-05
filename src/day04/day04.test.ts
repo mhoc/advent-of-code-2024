1fa3429eab1a3b713e7049a700f6359751b3702d
Mike Hockerman <mike@hockerman.com>
2024-12-04 21:45:22 -0500

    so fast

--- src/day04/day04.test.ts
+++ src/day04/day04.test.ts
@@ -1,77 +1,10 @@
 import { assertEquals } from "@std/assert";
-import { day04, Snowflake } from "./day04.ts";
+import { day04 } from "./day04.ts";
 
 Deno.test(function test_day04() {
   const data = Deno.readTextFileSync("./src/day04/input");
   const r = day04(data);
   assertEquals(r.xmasCount, 2483);
+  assertEquals(r.masmasCount, 1925);
   console.table(r);
-});
-
-const TEST_GRID = [
-  "QWERTYUI",
-  "OPASDFGH",
-  "JKLZXCVB",
-  "NMQWERTY",
-  "IOPASDFG",
-  "JKLZXCVB",
-  "NMQWERTY",
-  "UIOPASDF",
-];
-
-Deno.test(function test_snowflake_cc() {
-  const sf1 = new Snowflake(TEST_GRID, 4, 4);
-  assertEquals(sf1.cc(), "S");
-  const sf2 = new Snowflake(TEST_GRID, 20, 20);
-  assertEquals(sf2.cc(), undefined);
-});
-
-Deno.test(function test_snowflake_tt() {
-  const sf1 = new Snowflake(TEST_GRID, 4, 4);
-  assertEquals(sf1.tt(1), "E");
-  assertEquals(sf1.tt(2), "X");
-  assertEquals(sf1.tt(3), "D");
-  assertEquals(sf1.tt(4), "T");
-  const sf2 = new Snowflake(TEST_GRID, 20, 20);
-  assertEquals(sf2.tt(1), undefined);
-  const sf3 = new Snowflake(TEST_GRID, 0, 0);
-  assertEquals(sf3.tt(1), undefined);
-});
-
-Deno.test(function test_snowflake_tr() {
-  const sf1 = new Snowflake(TEST_GRID, 4, 4);
-  assertEquals(sf1.tr(1), "R");
-  assertEquals(sf1.tr(2), "V");
-  assertEquals(sf1.tr(3), "H");
-  assertEquals(sf1.tr(4), undefined);
-  const sf2 = new Snowflake(TEST_GRID, 20, 20);
-  assertEquals(sf2.tr(1), undefined);
-  const sf3 = new Snowflake(TEST_GRID, 0, 0);
-  assertEquals(sf3.tr(1), undefined);
-});
-
-Deno.test(function test_snowflake_rr() {
-  const sf1 = new Snowflake(TEST_GRID, 3, 3);
-  assertEquals(sf1.rr(1), "E");
-  assertEquals(sf1.rr(2), "R");
-  assertEquals(sf1.rr(3), "T");
-  assertEquals(sf1.rr(4), "Y");
-  assertEquals(sf1.rr(5), undefined);
-  const sf2 = new Snowflake(TEST_GRID, 20, 20);
-  assertEquals(sf2.rr(1), undefined);
-  const sf3 = new Snowflake(TEST_GRID, 7, 7);
-  assertEquals(sf3.rr(1), undefined);
-});
-
-Deno.test(function test_snowflake_br() {
-  const sf1 = new Snowflake(TEST_GRID, 3, 3);
-  assertEquals(sf1.br(1), "S");
-  assertEquals(sf1.br(2), "C");
-  assertEquals(sf1.br(3), "T");
-  assertEquals(sf1.br(4), "F");
-  assertEquals(sf1.br(5), undefined);
-  const sf2 = new Snowflake(TEST_GRID, 20, 20);
-  assertEquals(sf2.br(1), undefined);
-  const sf3 = new Snowflake(TEST_GRID, 7, 7);
-  assertEquals(sf3.br(1), undefined);
 });
--- src/day04/day04.ts
+++ src/day04/day04.ts
@@ -1,155 +1,89 @@
+// I went a little crazy on performance optimization on this one, which made it pretty ugly.
+
 export function day04(data: string) {
-  const lines = data.split("\n");
+  const grid = data.split("\n");
   let xmasCount = 0;
-  for (let lineno = 0; lineno < lines.length; lineno++) {
-    for (let charno = 0; charno < lines[lineno].length; charno++) {
-      const centerChar = lines[lineno][charno];
-      if (centerChar === "X") {
-        const wordflake = new Wordflake(new Snowflake(lines, lineno, charno));
-        xmasCount += wordflake
-          .wordsRadial(4)
-          .filter((w) => w === "XMAS").length;
+  for (let lineno = 0; lineno < grid.length; lineno++) {
+    for (let colno = 0; colno < grid[lineno].length; colno++) {
+      if (grid[lineno][colno] === "X") {
+        if (grid[lineno - 1]?.[colno] === "M") {
+          if (grid[lineno - 2]?.[colno] === "A") {
+            if (grid[lineno - 3]?.[colno] === "S") {
+              xmasCount++;
+            }
+          }
+        }
+        if (grid[lineno - 1]?.[colno + 1] === "M") {
+          if (grid[lineno - 2]?.[colno + 2] === "A") {
+            if (grid[lineno - 3]?.[colno + 3] === "S") {
+              xmasCount++;
+            }
+          }
+        }
+        if (grid[lineno]?.[colno + 1] === "M") {
+          if (grid[lineno]?.[colno + 2] === "A") {
+            if (grid[lineno]?.[colno + 3] === "S") {
+              xmasCount++;
+            }
+          }
+        }
+        if (grid[lineno + 1]?.[colno + 1] === "M") {
+          if (grid[lineno + 2]?.[colno + 2] === "A") {
+            if (grid[lineno + 3]?.[colno + 3] === "S") {
+              xmasCount++;
+            }
+          }
+        }
+        if (grid[lineno + 1]?.[colno] === "M") {
+          if (grid[lineno + 2]?.[colno] === "A") {
+            if (grid[lineno + 3]?.[colno] === "S") {
+              xmasCount++;
+            }
+          }
+        }
+        if (grid[lineno + 1]?.[colno - 1] === "M") {
+          if (grid[lineno + 2]?.[colno - 2] === "A") {
+            if (grid[lineno + 3]?.[colno - 3] === "S") {
+              xmasCount++;
+            }
+          }
+        }
+        if (grid[lineno]?.[colno - 1] === "M") {
+          if (grid[lineno]?.[colno - 2] === "A") {
+            if (grid[lineno]?.[colno - 3] === "S") {
+              xmasCount++;
+            }
+          }
+        }
+        if (grid[lineno - 1]?.[colno - 1] === "M") {
+          if (grid[lineno - 2]?.[colno - 2] === "A") {
+            if (grid[lineno - 3]?.[colno - 3] === "S") {
+              xmasCount++;
+            }
+          }
+        }
       }
     }
   }
   let masmasCount = 0;
-  for (let lineno = 0; lineno < lines.length; lineno++) {
-    for (let charno = 0; charno < lines[lineno].length; charno++) {
-      const centerChar = lines[lineno][charno];
+  for (let lineno = 1; lineno < grid.length - 1; lineno++) {
+    for (let colno = 1; colno < grid[lineno].length - 1; colno++) {
+      const centerChar = grid[lineno][colno];
       if (centerChar === "A") {
-        const wordflake = new Wordflake(new Snowflake(lines, lineno, charno));
-        const words = wordflake.wordsX3();
-        masmasCount +=
-          words.filter((w) => w === "MAS" || w === "SAM").length ===
-          words.length
-            ? 1
-            : 0;
+        const tl = grid[lineno - 1]?.[colno - 1];
+        const tr = grid[lineno - 1]?.[colno + 1];
+        const bl = grid[lineno + 1]?.[colno - 1];
+        const br = grid[lineno + 1]?.[colno + 1];
+        if (
+          (tl === "M" && br === "S" && tr === "M" && bl === "S") ||
+          (tl === "M" && br === "S" && tr === "S" && bl === "M") ||
+          (tl === "S" && br === "M" && tr === "M" && bl === "S") ||
+          (tl === "S" && br === "M" && tr === "S" && bl === "M")
+        ) {
+          masmasCount++;
+        }
       }
     }
   }
   return { masmasCount, xmasCount };
-}
-
-/**
- * Wordflake is an abstraction layer above Snowflake which forms words of a given length, in all 8 cardinal snowflake
- * directions.
- */
-export class Wordflake {
-  constructor(private snowflake: Snowflake) {}
-
-  public wordsRadial(length: number): string[] {
-    let tt = "";
-    for (let n = 0; n < length; n++) tt += this.snowflake.tt(n);
-    let tr = "";
-    for (let n = 0; n < length; n++) tr += this.snowflake.tr(n);
-    let rr = "";
-    for (let n = 0; n < length; n++) rr += this.snowflake.rr(n);
-    let br = "";
-    for (let n = 0; n < length; n++) br += this.snowflake.br(n);
-    let bb = "";
-    for (let n = 0; n < length; n++) bb += this.snowflake.bb(n);
-    let bl = "";
-    for (let n = 0; n < length; n++) bl += this.snowflake.bl(n);
-    let ll = "";
-    for (let n = 0; n < length; n++) ll += this.snowflake.ll(n);
-    let tl = "";
-    for (let n = 0; n < length; n++) tl += this.snowflake.tl(n);
-    return [tt, tr, rr, br, bb, bl, ll, tl];
-  }
-
-  public wordsX3(): string[] {
-    const tlbr = `${this.snowflake.tl(
-      1
-    )}${this.snowflake.cc()}${this.snowflake.br(1)}`;
-    const trbl = `${this.snowflake.tr(
-      1
-    )}${this.snowflake.cc()}${this.snowflake.bl(1)}`;
-    return [tlbr, trbl];
-  }
-}
-
-/**
- * A picture is worth a thousand words:
- *
- * tl3 --- --- tt3 --- --- tr3
- * --- tl2 --- tt2 --- tr2 ---
- * --- --- tl1 tt1 tr1 --- ---
- * ll3 ll2 ll1 cc0 rr1 rr2 rr3
- * --- --- bl1 bb1 br1 --- ---
- * --- bl2 --- bb2 --- br2 ---
- * bl3 --- --- bb3 --- --- br3
- */
-export class Snowflake {
-  constructor(
-    private grid: string[],
-    private lineno: number,
-    private colno: number
-  ) {
-    const linelen = grid[0].length;
-    for (const line of grid) {
-      if (line.length !== linelen) {
-        throw new Error(`grid lines must all be of equal length`);
-      }
-    }
-  }
-
-  cc() {
-    return this.grid.at(this.lineno)?.at(this.colno);
-  }
-
-  tt(f: number) {
-    return this.linebounded(this.lineno - f) && this.colbounded(this.colno)
-      ? this.grid.at(this.lineno - f)?.at(this.colno)
-      : undefined;
-  }
-
-  tr(f: number) {
-    return this.linebounded(this.lineno - f) && this.colbounded(this.colno + f)
-      ? this.grid.at(this.lineno - f)?.at(this.colno + f)
-      : undefined;
-  }
-
-  rr(f: number) {
-    return this.linebounded(this.lineno) && this.colbounded(this.colno + f)
-      ? this.grid.at(this.lineno)?.at(this.colno + f)
-      : undefined;
-  }
-
-  br(f: number) {
-    return this.linebounded(this.lineno + f) && this.colbounded(this.colno + f)
-      ? this.grid.at(this.lineno + f)?.at(this.colno + f)
-      : undefined;
-  }
-
-  bb(f: number) {
-    return this.linebounded(this.lineno + f) && this.colbounded(this.colno)
-      ? this.grid.at(this.lineno + f)?.at(this.colno)
-      : undefined;
-  }
-
-  bl(f: number) {
-    return this.linebounded(this.lineno + f) && this.colbounded(this.colno - f)
-      ? this.grid.at(this.lineno + f)?.at(this.colno - f)
-      : undefined;
-  }
-
-  ll(f: number) {
-    return this.linebounded(this.lineno) && this.colbounded(this.colno - f)
-      ? this.grid.at(this.lineno)?.at(this.colno - f)
-      : undefined;
-  }
-
-  tl(f: number) {
-    return this.linebounded(this.lineno - f) && this.colbounded(this.colno - f)
-      ? this.grid.at(this.lineno - f)?.at(this.colno - f)
-      : undefined;
-  }
-
-  private colbounded(i: number): boolean {
-    return i >= 0 && i < this.grid[0].length;
-  }
-
-  private linebounded(i: number): boolean {
-    return i >= 0 && i < this.grid.length;
-  }
 }
